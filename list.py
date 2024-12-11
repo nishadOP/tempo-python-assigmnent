0ae5aec4c70b4486e227d2d5809c3c2530f3c88a
nishadOP <nishadjavedhaque@gmail.com>
2024-12-11 12:35:25 +0530

    updated

--- list.py
+++ list.py
@@ -1,16 +1,16 @@
-
+# Write a Python program to find the difference between the largest and smallest elements in a list.
 
 
 # function  for checkinh the list 
 def find_difference(lst):
     if len(lst) == 0:
         return 0  # Return 0 if the list is empty
-    smallest = min(lst)
-    largest = max(lst)
-    difference = largest - smallest
+    smallest = min(lst) #4
+    largest = max(lst) #23
+    difference = largest - smallest #19
     return difference
 
 # Example usage
-numbers = [10, 4, 7, 23, 15, 8]
+numbers = [10, 4, 7, 23, 15, 8] #5
 diff = find_difference(numbers)
 print("The difference between the smallest and largest elements is:", diff)
